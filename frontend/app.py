76bff2e5114f284e1c1132a62ffbdfa27be0ae9f
ashrithjacob <ashrithjacob@gmail.com>
2023-05-14 19:44:09 +0200

    add port 10000 to api call

--- frontend/app.py
+++ frontend/app.py
@@ -10,7 +10,7 @@ st.write('Simply upload your pdf file and get a page by page summary!')
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
     file = {'file': uploaded_file.getvalue()}
-    response = requests.post("http://localhost:8000/upload", files=file)
+    response = requests.post("http://0.0.0.0:10000/upload", files=file)
     if response.status_code == 200:
         st.write("File uploaded successfully!")
         #st.write(response.json())
@@ -21,7 +21,7 @@ if st.button('Summarize page by page'):
     status = True
     page_number = 0
     while status:
-        response = requests.get("http://localhost:8000/summary", params={"page_number": page_number})
+        response = requests.get("http://0.0.0.0:10000/summary", params={"page_number": page_number})
         if response.status_code == 200:
             st.write("Page number: ", page_number)
             st.write(response.json()["summary"])

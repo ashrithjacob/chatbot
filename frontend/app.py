bcb2084c8f044960a257562d8badf88e37841260
ashrithjacob <ashrithjacob@gmail.com>
2023-06-10 18:48:17 +0200

    change frontend text

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 
 st.title('AI POWERED PDF SUMMARY!')
-st.write('Simply upload your PDF file and get a page by page summary!')
+st.write('Simply upload your .pdf file and get a page by page summary!')
 
 uploaded_file = st.file_uploader('Choose your pdf file', type="pdf")
 if uploaded_file is not None:

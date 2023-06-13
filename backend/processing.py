d7619ee366bb4feac58a3b20e9b876bb144e6d91
ashrithjacob <ashrithjacob@gmail.com>
2023-06-13 17:05:28 +0200

    checking for None status

--- backend/processing.py
+++ backend/processing.py
@@ -26,7 +26,7 @@ class Summarizer:
         load_dotenv()
         page = Summarizer.get_page(pdf_loader, page_number)
         page_summary = Summarizer.page_summary(page)
-        env = os.environ.get('OPENAI_API_KEY')
-        if env is None:
-            env = "ISNONE"
+        #env = os.environ.get('OPENAI_API_KEY')
+        #if env is None:
+        env = "ISNONE"
         return page_summary, env

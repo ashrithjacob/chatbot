8ae56532da0182f11ed9e904caabf7a38cd0abc6
ashrithjacob <ashrithjacob@gmail.com>
2023-06-13 17:09:25 +0200

    checking for None status

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ def get_summary(page_number: int):
         page_summary, env = Summarizer.summary(pdfreader, page_number)
     else:
         raise HTTPException(status_code=404, detail="Page not found")
-    return {"summary":page_summary, "env": env}
+    return {"summary":page_summary, "env":env}
 
 @app.get("/rawtext")
 def get_rawtext(page_number: int):
